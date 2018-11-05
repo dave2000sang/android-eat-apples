96e5a4648f0c0025ece7ecd36714ab58b92d1367
1298se <40322963+1298se@users.noreply.github.com>
2018-11-04 22:10:46 -0500

    Add files via upload
    Copy pasta this

---
+++ base.py
@@ -0,0 +1,70 @@
+import pygame
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+jet_width = 208
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('fuckblyat')
+clock = pygame.time.Clock()
+
+jetImg = pygame.image.load('fighterjet.png')
+
+def jet(x, y):
+    gameDisplay.blit(jetImg, (x, y))
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center =  ((display_width /2), (display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+def crash():
+    message_display('blyat')
+
+def game_loop():
+    x = (display_width * 0.4)
+    y = (display_height * 0.5)
+
+    x_change = 0
+
+    gameExit = False
+
+    while not gameExit:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+                print(event)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+            if event.type == pygame.KEYUP :
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x += x_change
+        gameDisplay.fill(white)
+        jet(x, y)
+
+        if x > display_width - jet_width or x < 0:
+            crash()
+
+        pygame.display.update()
+        clock.tick(60)
+
+game_loop()
+pygame.quit()
+quit()
