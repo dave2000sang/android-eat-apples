f3ec7a6891d09db110ac4acf9bacbe1cef49c8bd
Kevinwen1999 <cybergodzilla1999@gmail.com>
2018-11-17 17:37:42 -0500

    Updated blyat.py

--- blyat.py
+++ blyat.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+import collections
 
 def rescale_frame(frame, factor = 1.75):
     width = int(frame.shape[1] * factor)
@@ -16,14 +17,22 @@ cascPath2 = "eyesblyat.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 eyesCascade = cv2.CascadeClassifier(cascPath2)
 
+# Initiate video capturing
 videoCapture = cv2.VideoCapture(0)
 videoCapture.set(3, 800)
 videoCapture.set(4, 600)
 
+# Declare center-position queue
+cpQueue = collections.deque()
+
+# Record last face position
+lastFace = (800 / 2, 600 / 2, 30, 30)
+
 def process_frame():
+    global lastFace
+
     # Read the frame:
     ret, frame = videoCapture.read()
-
 
     #frame = rescale_frame(frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -32,17 +41,36 @@ def process_frame():
     # Detect faces in frame
     faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
+        scaleFactor=1.05,
+        minNeighbors=6,
         minSize=(30, 30)
         #flags = cv2.CV_HAAR_SCALE_IMAGE
     )
+
+    # Show faces recognized on picture
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Update position queue
+    if len(faces):
+        lastFace = faces[0]
+    else:
+        faces = []
+        faces.append(lastFace)
+
+    cpQueue.append((faces[0][0] + faces[0][2] / 2, faces[0][1] + faces[0][3] / 2))
+
+    if len(cpQueue) > 20:
+        cpQueue.popleft()
+    for (x, y) in cpQueue:
+        cv2.circle(frame, (int(x), int(y)), 2, (0, 0, 255), 5)
+
+    # write frame to image
     cv2.imwrite("blyatface.jpg", frame)
+
     return faces
 
-    #print("Found {0} faces!".format(len(faces)))
+    # print("Found {0} faces!".format(len(faces)))
 
     # Draw a rectangle around the faces
     '''
