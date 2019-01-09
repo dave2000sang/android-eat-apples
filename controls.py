2f47fc0d0be923d4c5124885141072f28bead106
1298se <oliversong7h@gmail.com>
2019-01-09 16:29:45 -0500

    completed instructions menu

--- controls.py
+++ controls.py
@@ -6,10 +6,14 @@ import startmenu
 
 pygame.init()
 
+
 def instructions():
     display = True
     box_height = 500
     box_width = 400
+
+
+    instruct_image = pygame.image.load("instructions.png").convert()
 
     while display:
         for event in pygame.event.get():
@@ -27,7 +31,18 @@ def instructions():
         box.blit(titleSurf, titleRect)
 
         # Instructions Text
+        instructSurf, instructRect = text.text_objects("Move your face to catch!", text.smallText, colours.black)
+        instructSurf2, instructRect2 = text.text_objects("the falling apples for points!", text.smallText, colours.black)
+        instructRect.center = ((box_width/2), 125)
+        instructRect2.center = ((box_width/2), 175)
+        box.blit(instructSurf, instructRect)
+        box.blit(instructSurf2, instructRect2)
 
+
+        # Instructions picture
+        box.blit(instruct_image, (50, 250))
+
+        # Back button
         text.button(base.gameDisplay, "Back", 50, 500, 100, 50, colours.lightgray, colours.gray, startmenu.game_intro)
 
         pygame.draw.rect(box, colours.black, (0, 0, box_width, box_height), 3)
